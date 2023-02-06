8e698aef9b063d1c274b63cd6dec3873f3fcbf98
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-06 17:15:17 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Homepage from './features/homepage/Homepage';
+import Gainers from './features/gainers/Gainers';
+import Losers from './features/losers/Losers';
+import Movers from './features/movers/Movers';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Homepage />} />
+        <Route path="gainers" element={<Gainers />} />
+        <Route path="losers" element={<Losers />} />
+        <Route path="movers" element={<Movers />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
