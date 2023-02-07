c606dccfd1072ade02834fe84b80142ba507847b
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-07 15:48:24 +0300

    Remove extra routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Homepage from './features/homepage/Homepage';
-import Gainers from './features/gainers/Gainers';
-import Losers from './features/losers/Losers';
-import Movers from './features/movers/Movers';
+import Details from './features/homepage/Details';
 import './App.css';
 
 const App = () => (
@@ -10,9 +8,7 @@ const App = () => (
     <Routes>
       <Route path="/">
         <Route index element={<Homepage />} />
-        <Route path="gainers" element={<Gainers />} />
-        <Route path="losers" element={<Losers />} />
-        <Route path="movers" element={<Movers />} />
+        <Route path="details" element={<Details />} />
       </Route>
     </Routes>
   </BrowserRouter>
