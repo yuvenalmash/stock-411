8589cc12ebecb0585b3ae206fabb4974e5375750
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-07 15:50:35 +0300

    Use state data to create cards

--- src/features/homepage/Homepage.js
+++ src/features/homepage/Homepage.js
@@ -1,13 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { selectAllQuotes, selectState, fetchQoutes } from './homepageSlice';
+import Card from './Card';
 
-const Homepage = () => (
-  <>
-    <h1>Home Page</h1>
-    <Link to="/">Homepage</Link>
-    <Link to="gainers">Gainers</Link>
-    <Link to="losers">Losers</Link>
-    <Link to="movers">Movers</Link>
-  </>
-);
+const Homepage = () => {
+  const dispatch = useDispatch();
+  const status = useSelector(selectState);
+  const contents = useSelector(selectAllQuotes);
+  useEffect(() => {
+    if (contents.length === 0 && status === 'idle') {
+      dispatch(fetchQoutes());
+    }
+  });
+  const quotes = useSelector(selectAllQuotes);
+  console.log(`quotes = ${JSON.stringify(quotes)}`);
+  return (
+    <>
+      <h1>Home Page</h1>
+      <ul>
+        {quotes.map((quote) => (
+          <Card
+            key={quote['01. symbol']}
+            quote={quote}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default Homepage;
