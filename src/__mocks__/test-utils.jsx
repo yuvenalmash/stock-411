e1e405f79b98928e1f1b0ae59ccb3cf2c3ff9dcc
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-02-08 18:43:48 +0300

    mock redux store

---
+++ src/__mocks__/test-utils.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+// As a basic setup, import your same slice reducers
+import { homepageSlice } from '../features/homepage/homepageSlice';
+
+
+export function renderWithProviders(
+  ui,
+  {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({
+      reducer: { homepage: homepageSlice.reducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return (
+      <Provider store={store}>
+        {children}
+      </Provider>
+    )
+  }
+
+  // Return an object with the store and all of RTL's query functions
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
