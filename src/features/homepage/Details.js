a68eed5f07167568a95b06fff4671c7e3ac1e642
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-07-12 14:52:00 +0300

    remove unused icons

--- src/features/homepage/Details.js
+++ src/features/homepage/Details.js
@@ -1,6 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import {
-  BsMic, BsGear, BsChevronLeft, BsArrowRightCircle,
+  BsChevronLeft,
 } from 'react-icons/bs';
 import { Img } from './Card';
 import './css/Homepage.css';
@@ -9,15 +9,26 @@ import './css/Details.css';
 const Details = () => {
   const location = useLocation();
   const quote = location.state;
+
+  const lastTradeTime = `${quote.last_trade_time.slice(0, 10)} ${quote.last_trade_time.slice(11, 19)}`;
+
+  const quoteItems = {
+    Currency: quote.currency,
+    Price: quote.price,
+    Open: quote.day_open,
+    High: quote.day_high,
+    Low: quote.day_low,
+    Change: quote.day_change,
+    Volume: quote.volume,
+    'Last trade time': lastTradeTime,
+    'Previouse close': quote.previous_close_price,
+  };
   return (
     <main className="details">
       <nav id="details-nav">
-        <Link to="/stock-411/"><BsChevronLeft className="navIcon" /></Link>
+        <Link to="/stock-411/" id="button"><BsChevronLeft className="navIcon" /></Link>
         Company Details
-        <div id="rightNav">
-          <BsMic className="navIcon" />
-          <BsGear className="navIcon" />
-        </div>
+        <div id="rightNav" />
       </nav>
       <header id="home-header">
         <div className="headerImg">
@@ -30,78 +41,21 @@ const Details = () => {
         </h2>
       </header>
       <div id="separator"><h3>COMPANY BREAKDOWN</h3></div>
-      <ul id="details-list">
-        <li>
-          <p>Currency:</p>
-          <div className="listRightSec">
-            <p>{quote.currency}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>Price:</p>
-          <div className="listRightSec">
-            <p>{quote.price}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>Open:</p>
-          <div className="listRightSec">
-            <p>{quote.day_open}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>High:</p>
-          <div className="listRightSec">
-            <p>{quote.day_high}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>Low:</p>
-          <div className="listRightSec">
-            <p>{quote.day_low}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>Change:</p>
-          <div className="listRightSec">
-            <p>{quote.day_change}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>Volume:</p>
-          <div className="listRightSec">
-            <p>{quote.volume}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>Last trade time:</p>
-          <div className="listRightSec">
-            <p>{quote.last_trade_time}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>Previouse close:</p>
-          <div className="listRightSec">
-            <p>{quote.previous_close_price}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-        <li>
-          <p>Exchange:</p>
-          <div className="listRightSec">
-            <p>{quote.exchange_short}</p>
-            <BsArrowRightCircle id="enterIcon" />
-          </div>
-        </li>
-      </ul>
+      <div id="details-list-container">
+        <ul id="details-list">
+          {Object.keys(quoteItems).map((key) => (
+            <li key={key}>
+              <p>
+                {key}
+                :
+              </p>
+              <div className="listRightSec">
+                <p>{quoteItems[key]}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 };
